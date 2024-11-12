16948555ce4536542c539f92b64a4f40a8f34625
Sharachel <kimyen.pham01@gmail.com>
2024-11-12 10:40:35 +0100

    add import in files

---
+++ site-mayhumanlab/src/features/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import filmReducer from './film/filmSlice'
+
+const store = configureStore({
+    reducer:{
+        
+    },
+});
+
+export default store;
