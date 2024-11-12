246f24a83130729db83f9779093e526540c9d2a6
Elea <Elea.Poirot.Etu@univ-lemans.fr>
2024-11-12 17:46:15 +0100

    modif slice

--- site-mayhumanlab/src/features/project/projectSlice.js
+++ site-mayhumanlab/src/features/project/projectSlice.js
@@ -1,1 +1,16 @@
 import { createSlice } from '@reduxjs/toolkit';
+
+const slice = createSlice({
+    name: 'slice',
+    initialState: {
+            //states
+    },
+    reducers: {
+            //reducers
+    },
+    extraReducers:(builder)=>{
+            //ajout des cas
+    }
+})
+
+export default slice.reducer;
