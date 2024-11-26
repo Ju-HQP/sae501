9df084153b6ad2bc710f1eaa8fdeb31fa48f6c5e
Sharachel <kimyen.pham01@gmail.com>
2024-11-26 11:10:57 +0100

    new branch trombinoscope

--- site-mayhumanlab/src/features/volunteer/volunteerAsyncAction.js
+++ site-mayhumanlab/src/features/volunteer/volunteerAsyncAction.js
@@ -7,27 +7,66 @@ import {
 
 //fonctions asynchrones pourcommuniquer avec l'api
 
-export const saveVolunteer = createAsyncThunk(
-    'benevoles/saveBenevole',
-    (datas,{
-        dispatch,
-        getState
-    }) => {
-        const id= getState().idVolunteerModifying
-        if(id){
-            dispatch(updateVolunteer(datas));
-        } else {
-            dispatch(addVolunteer(datas));
-        }
+export const loadProjects = createAsyncThunk(
+    'benevoles/loadProjects',
+    async (datas, { rejectWithValue }) => {
+        try{
+            const response = await fetch(URL_API_PROJETCS, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(datas),
+            });
+            return await response.json();
+        }catch (error){
+            return rejectWithValue("L'application est actuellement indisponible. Veuillez réessayer ultérieurement");
+        };
+    }
+)
+
+export const loadActus = createAsyncThunk(
+    'benevoles/loadActus',
+    async (datas, { rejectWithValue }) => {
+        try{
+            const response = await fetch(URL_API_ACTUS, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(datas),
+            });
+            return await response.json();
+        }catch (error){
+            return rejectWithValue("L'application est actuellement indisponible. Veuillez réessayer ultérieurement");
+        };
+    }
+)
+
+export const loadVolunteer = createAsyncThunk(
+    'benevoles/loadVolunteer',
+    async (datas, { rejectWithValue }) => {
+        try{
+            const response = await fetch(URL_API_VOLUNTEERS, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(datas),
+            });
+            return await response.json();
+        }catch (error){
+            return rejectWithValue("L'application est actuellement indisponible. Veuillez réessayer ultérieurement");
+        };
     }
 )
 
 export const addVolunteer = createAsyncThunk(
-    'benevoles/creerCompteBenevole',
-    async (datas) => {
+    'benevoles/addVolunteer',
+    async (datas, { rejectWithValue }) => {
         try {
-            const res = await fetch(URL_API, {
-                method: 'POST',
+            const res = await fetch(URL_API_VOLUNTEERS, {
+                mmethod: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
                 },
@@ -44,9 +83,37 @@ export const addVolunteer = createAsyncThunk(
 )
 
 export const updateVolunteer = createAsyncThunk(
-
+    'benevoles/updateVolunteer',
+    async (datas, { rejectWithValue }) => {
+        try {
+            const res = await fetch(URL_API_VOLUNTEERS, {
+                mmethod: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(datas),
+            });
+            return await response.json();
+        } catch (errorAxio){
+            return rejectWithValue(errorAxio.response.data.error.message);
+        };
+    }
 )
 
 export const deleteVolunteer = createAsyncThunk(
-
+    'benevoles/deleteVolunteer',
+    async (datas, { rejectWithValue }) => {
+        try {
+            const res = await fetch(URL_API_VOLUNTEERS, {
+                mmethod: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(datas),
+            });
+            return await response.json();
+        }catch(errorAxio){
+            return rejectWithValue(errorAxio.response.data.error.message);
+        };
+    }
 )
