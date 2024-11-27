5b947c4a116cc370adfd859a0c73f5bdc299af9d
Sharachel <kimyen.pham01@gmail.com>
2024-11-27 10:04:14 +0100

    correction

--- site-mayhumanlab/src/components/VolunteerListItem.jsx
+++ site-mayhumanlab/src/components/VolunteerListItem.jsx
@@ -1,23 +1,10 @@
 import React from "react";
-import { useDispatch } from 'react-redux';
 
-function VolunteerListItem({ volunteer }){
+function VolunteerListItem({}){
     
     return(
         <>
-            <div className="grid lg:grid-cols-2">
-                <div>
-                    <img src={volunteer.photo_b} alt={`${"Photo de "}+${volunteer.prenom_b}+${" "}+${volunteer.nom_b}`}/>
-                    <p className="text-base lg:text-lg">{volunteer.prenom_b} {volunteer.nom_b}</p>
-                </div>
-                <div>
-                    <p className="text-md lg:text-base font-semibold">Coordonnées</p>
-                    <ul>
-                        <li>{volunteer.mail_b}</li>
-                        <li>{volunteer.tel_b}</li>
-                    </ul>
-                </div>
-            </div>
+            
         </>
     )
 };
--- site-mayhumanlab/src/components/VolunteersChartItem.jsx
+++ site-mayhumanlab/src/components/VolunteersChartItem.jsx
@@ -1,6 +1,24 @@
-function VolunteersItem(){
+import React from "react";
 
-    return(<></>)
+function VolunteersItem({ volunteer }){
+
+    return(
+        <>
+            <div className="grid lg:grid-cols-2 lg:mx-64">
+                <div>
+                    <img src={volunteer.photo_b} className="w-24" alt={`${"Photo de "}+${volunteer.prenom_b}+${" "}+${volunteer.nom_b}`}/>
+                    <p className="text-base lg:text-lg">{volunteer.prenom_b} {volunteer.nom_b}</p>
+                </div>
+                <div>
+                    <p className="text-md lg:text-base font-semibold">Coordonnées</p>
+                    <ul>
+                        <li>{volunteer.mail_b}</li>
+                        <li>{volunteer.tel_b}</li>
+                    </ul>
+                </div>
+            </div>
+        </>
+        )
 };
 
 export default VolunteersItem;
