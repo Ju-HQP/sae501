0dbce64c6c17ff3cfb1cfdece31637ce3a0f50ec
Elea <Elea.Poirot.Etu@univ-lemans.fr>
2024-11-27 09:34:15 +0100

    test

--- site-mayhumanlab/src/features/volunteer/volunteerAsyncAction.js
+++ site-mayhumanlab/src/features/volunteer/volunteerAsyncAction.js
@@ -1,11 +1,25 @@
-import {
-    createAsyncThunk
-} from '@reduxjs/toolkit';
-import {
-    URL_API
-} from '../../utils/config.js';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { URL_API_ACTUS, URL_API_PROJECTS, URL_API_VOLUNTEERS, test } from '../../utils/config.js';
 
 //fonctions asynchrones pourcommuniquer avec l'api
+
+export const loadVolunteer = createAsyncThunk(
+    'benevoles/loadVolunteer',
+    async (_, { rejectWithValue }) => {
+        try{
+            const response = await fetch(URL_API_VOLUNTEERS, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+            console.log("ok", response.json());
+            return await response.json();
+        }catch (error){
+            return rejectWithValue("L'application est actuellement indisponible. Veuillez réessayer ultérieurement");
+        };
+    }
+)
 
 export const saveVolunteer = createAsyncThunk(
     'benevoles/saveBenevole',
@@ -23,17 +37,17 @@ export const saveVolunteer = createAsyncThunk(
 )
 
 export const addVolunteer = createAsyncThunk(
-    'benevoles/creerCompteBenevole',
+    'benevoles/addVolunteer',
     async (datas) => {
         try {
-            const res = await fetch(URL_API, {
+            const res = await fetch(URL_API_VOLUNTEERS, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
                 },
                 body: JSON.stringify(datas),
             });
-            return res.data;
+            return await res.json();
         } catch (er) {
             return {
                 success: false,
@@ -44,9 +58,37 @@ export const addVolunteer = createAsyncThunk(
 )
 
 export const updateVolunteer = createAsyncThunk(
-
+    'benevoles/updateVolunteer',
+    async (datas, { rejectWithValue }) => {
+        try {
+            const queryString = new URLSearchParams(datas).toString();
+            const response = await fetch(`${URL_API_VOLUNTEERS}?${queryString}`, {
+                mmethod: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+            return await response.json();
+        } catch (errorAxio){
+            return rejectWithValue(errorAxio.response.data.error.message);
+        };
+    }
 )
 
 export const deleteVolunteer = createAsyncThunk(
-
+    'benevoles/deleteVolunteer',
+    async (datas, { rejectWithValue }) => {
+        try {
+            const queryString = new URLSearchParams(datas).toString();
+            const response = await fetch(`${URL_API_VOLUNTEERS}?${queryString}`, {
+                mmethod: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+            return await response.json();
+        }catch(errorAxio){
+            return rejectWithValue(errorAxio.response.data.error.message);
+        };
+    }
 )
