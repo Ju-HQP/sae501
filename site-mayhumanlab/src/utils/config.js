cffdb6103b8697745ac8bd0a2244c92f0926ee3e
Elea <Elea.Poirot.Etu@univ-lemans.fr>
2024-11-27 10:05:06 +0100

    bniobrts

--- site-mayhumanlab/src/utils/config.js
+++ site-mayhumanlab/src/utils/config.js
@@ -1,4 +1,4 @@
-export const URL_API_VOLUNTEERS = '/volunteers.json';
+export const URL_API_VOLUNTEERS = '/site-mayhumanlab/public/volunteers.json';
 export const URL_API_PROJECTS = '/';
 export const URL_API_ACTUS = '/';
 export const test = "oui";
