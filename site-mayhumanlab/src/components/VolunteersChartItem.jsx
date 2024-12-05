029a6ad49e0a1cd2dc701b0b13ccdfce5d4fe9bc
Ju-HQP <julien.hedreul.etu@univ-lemans.fr>
2024-12-05 16:31:56 +0100

    debut modif css trombi

--- site-mayhumanlab/src/components/VolunteersChartItem.jsx
+++ site-mayhumanlab/src/components/VolunteersChartItem.jsx
@@ -5,11 +5,11 @@ function VolunteersItem({ volunteer }){
     return(
         <>
             <div className="grid md:grid-cols-2 gap-2 lg:gap-14 m-6">
-                <div className="flex flex-col items-center gap-1">
+                <div id="volunteer_bloc1" className="flex flex-col items-center gap-1">
                     <img src={volunteer.photo_b} className="object-contain w-40 h-40 rounded-lg" alt={`${"Photo de "}+${volunteer.prenom_b}+${" "}+${volunteer.nom_b}`}/>
                     <p className="text-lg text-center">{volunteer.prenom_b} {volunteer.nom_b}</p>
                 </div>
-                <div className="m-auto">
+                <div id="volunteer_bloc2" className="flex flex-col space-y-4">
                     <div className="my-1">
                         <p className="text-base text-center font-semibold">Coordonnées</p>
                         <ul className="text-center">
--- site-mayhumanlab/src/pages/VolunteersChart.jsx
+++ site-mayhumanlab/src/pages/VolunteersChart.jsx
@@ -1,37 +1,41 @@
 import { useDispatch, useSelector } from "react-redux";
-import { selectLoading, selectVolunteer } from "../features/volunteer/volunteerSelector";
+import {
+  selectLoading,
+  selectVolunteer,
+} from "../features/volunteer/volunteerSelector";
 import VolunteersChartItem from "../components/VolunteersChartItem";
 import { useEffect } from "react";
 import { loadVolunteer } from "../features/volunteer/volunteerAsyncAction";
 import Header from "../components/Header";
-import Grid from '@mui/material/Grid2';
-
 
 function VolunteersChart() {
+  const dispatch = useDispatch();
+  const loading = useSelector(selectLoading);
+  const volunteerList = useSelector(selectVolunteer);
 
-    const dispatch = useDispatch();
-    const loading = useSelector(selectLoading);
-    const volunteerList = useSelector(selectVolunteer);
+  useEffect(() => {
+    dispatch(loadVolunteer());
+  }, []);
 
-    useEffect(() => {
-        dispatch(loadVolunteer());
-    }, []);
-
-    return (
-        <>
-            <Header />
-            <main>
-                <h1 className="font-bold text-xl text-center">Trombinoscope</h1>
-                <div className="grid lg:grid-cols-2">
-                    {loading ? <p>Chargement des données...</p> : volunteerList.map((volunteer) => 
-                        <Grid item xs={1} sm={1} md={2} ld={2} key={volunteer.id}>
-                        <VolunteersChartItem volunteer={volunteer} />
-                        </Grid>
-                    )}
-                </div>
-            </main>
-        </>
-    )
-};
+  return (
+    <>
+      <Header />
+      <main>
+        <h1 className="font-bold text-xl text-center">Trombinoscope</h1>
+        <div className="grid grid-cols-1 md:grid-cols-2">
+          {loading ? (
+            <p>Chargement des données...</p>
+          ) : (
+            volunteerList.map((volunteer) => (
+              <div>
+                <VolunteersChartItem volunteer={volunteer} />
+              </div>
+            ))
+          )}
+        </div>
+      </main>
+    </>
+  );
+}
 
 export default VolunteersChart;
