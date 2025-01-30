78401e634409b3953f10c7b16a0ca33dfcd8f105
obiney <oriane.biney@gmail.com>
2025-01-30 18:11:17 +0100

    commit

--- site-mayhumanlab/src/components/Footer.jsx
+++ site-mayhumanlab/src/components/Footer.jsx
@@ -2,7 +2,7 @@ function Footer({ contactVisible }) {
     return (
         <>
 
-            <footer className="w-full px-8 py-4 flex flex-col justify-between z-50 bg-gray-800 font-roboto text-white">
+            <footer className="w-full px-8 py-4 flex flex-col justify-between z-50 bg-sky-800 font-roboto text-white">
                 {
                     contactVisible
                     &&
