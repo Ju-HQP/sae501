063fc4532a0929251f3ddc327cebfd81148ad9d8
Ju-HQP <julien.hedreul.etu@univ-lemans.fr>
2025-01-31 03:48:37 +0100

    Touche entrée pour connexion sans succès

--- site-mayhumanlab/src/pages/ConnectionForm.jsx
+++ site-mayhumanlab/src/pages/ConnectionForm.jsx
@@ -4,14 +4,12 @@ import { Field, Form } from "react-final-form";
 import { required } from "../utils/validators";
 import { ExclamationTriangleIcon } from "@heroicons/react/24/outline";
 import { login } from "../features/user/connexion.js";
-import { selectErrorLogin, selectUserIsConnected } from "../features/user/userSelector.js";
-import { useNavigate, Link } from "react-router-dom";
+import { selectErrorLogin } from "../features/user/userSelector.js";
+import { Link } from "react-router-dom";
 
 function ConnectionForm() {
   const dispatch = useDispatch();
-  const navigate = useNavigate();
   const errorLogin = useSelector(selectErrorLogin);
-  const isConnected = useSelector(selectUserIsConnected);
 
   const handleExit = () => {
     dispatch(stopConnecting());
